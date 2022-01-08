53b5eec359dedbf3b62609f4a1673c8af6cc8fa5
RESPULTE <yeapzing@gmail.com>
2022-01-09 02:00:02 +0800

    I've honestly thought of punching a hole in my comp a terrifying amount of times after finding and debugging rogue infinite while loop for 5 hours str8, should almost be done now, some more clean up with the overall structure of the code and a few more user-friendly methods and that should do it. Note-to-self: never trust a while-loop >>:(

--- src/pytree/quadtree.py
+++ src/pytree/quadtree.py
@@ -81,12 +81,12 @@ class QuadNode:
 
     @property
     def is_leaf(self):
-        return self.total_entity >= 0
+        return self.total_entity >= 0 if self.in_use else False
      
 
     @property
     def in_use(self):
-        return self.first_child != -1 and self.total_entity >=0 and self.parent_index != -1
+        return self.parent_index != -2
 
 
     def update(self, **kwargs) -> None:
@@ -103,7 +103,7 @@ class QuadNode:
 
 class QuadTree:
 
-    def __init__(self, bbox: BBox, capacity: int=2, max_depth: int=8, auto_id: bool=False):
+    def __init__(self, bbox: BBox, capacity: int=2, max_depth: int=12, auto_id: bool=False):
 
         if not isinstance(bbox, (type(None), tuple)) or not isinstance(bbox[0],  (int, float)):
             raise ValueError(f"bounding box of entity must be a tuple of 4 numbers ")
@@ -116,8 +116,11 @@ class QuadTree:
         self.max_depth = max_depth
         self.auto_id   = auto_id
 
-        self._free_node_index        = -1
+        self._free_quad_node_index   = -1
         self._free_entity_node_index = -1
+
+        self.num_quad_node_in_use   = 1
+        self.num_entity_node_in_use = 0
 
         self.all_entity:      Dict[UID, 'QuadEntity'] = {}
         self.all_entity_node: List[QuadEntityNode] = []
@@ -128,26 +131,17 @@ class QuadTree:
 
     @property
     def real_depth(self):
-        return (len(self.all_quad_node) - 1) / 4
+        return (len(self.all_quad_node) - 1) // 4
+
 
     @property
     def depth(self):
-        return (self.num_quad_node_in_use - 1) / 4
+        return (self.num_quad_node_in_use - 1) // 4
 
 
     @property
     def root(self):
         return self.all_quad_node[0]
-    
-
-    @property
-    def num_quad_node_in_use(self):
-        return sum(1 for qn in self.all_quad_node if qn.in_use)
-
-
-    @property
-    def num_entity_node_in_use(self):
-        return sum(1 for en in self.all_entity_node if en.in_use)
 
 
     @classmethod
@@ -174,7 +168,7 @@ class QuadTree:
         quadrants   = [(qnode_index - 1) % 4]
 
         depth = 0
-        while qnode.parent_index != -1:
+        while qnode.parent_index >= 0:
             quadrants.append((qnode.parent_index - 1) % 4)
             qnode = self.all_quad_node[qnode.parent_index]
             depth += 1
@@ -192,8 +186,8 @@ class QuadTree:
         tbbox_area = get_box_area(tbbox)
         to_process = [(0, self.bbox)]
 
-        while to_process:       
-        
+        while to_process:           
+           
             qindex, qbbox = to_process.pop()
             qnode = self.all_quad_node[qindex]
 
@@ -230,11 +224,11 @@ class QuadTree:
         bounded_leaves = []
 
         while to_process:       
-            
+
             qindex, qbbox = to_process.pop()
             qnode = self.all_quad_node[qindex]
         
-            if qnode.total_entity >= 0: 
+            if qnode.in_use: 
                 quad_data = (qindex, qnode, qbbox) if index else (qnode, qbbox)
                 bounded_leaves.append(quad_data)
                 continue
@@ -257,7 +251,7 @@ class QuadTree:
             if qnode.total_entity <= 0: return []
 
             leaf_nodes = self.find_leaves(qnode=qnode)
-
+            
             indexed_entity_nodes = []
 
             for leaf, _ in leaf_nodes:
@@ -270,7 +264,7 @@ class QuadTree:
 
                     indexed_entity_nodes.append(entity_data)
                     entity_node_index = entity_node.next_index
-                
+
             return indexed_entity_nodes
 
         def find_entity_node_by_eid(eid):
@@ -280,7 +274,7 @@ class QuadTree:
 
         if qnode == None and eid== None: 
             raise ValueError("at least one or the other is required, 'qnode' or 'eid'")
-        if qnode  != None and eid != None: 
+        if qnode != None and eid != None: 
             raise ValueError("only one or the other is allowed, 'qnode' or 'eid'")
 
         if qnode != None: return find_entity_node_by_node(qnode)
@@ -307,82 +301,78 @@ class QuadTree:
     def delete(self, eid: UID) -> None:
         if eid not in self.all_entity:
             raise ValueError(f"{eid} is not in the entity_list")
+        self.set_free_entity_nodes(self.find_entity_node(eid=eid, index=True))
 
-        [self.set_free(en) for en in self.find_entity_node(eid=eid)]
 
+    def clean_up(self) -> None:
 
-    def cleanup(self) -> None:
-        node_index_to_process = []
+        def traversal_clean_up(node: QuadNode) -> None:
+            if node.first_child != -1 and node.in_use and not node.is_leaf:
+                if sum(traversal_clean_up(self.all_quad_node[node.first_child + i]) for i in range(4)) == 4:
+                    self.set_free_quad_node(self.all_quad_node[node.first_child])
+                    if node == self.root: node.first_child = 1
 
-        if self.root.first_child != -1:
-            node_index_to_process.append(0)
+            if node.total_entity == 0 and node.parent_index != -2:                
+                return 1
 
-        while node_index_to_process:
+            return 0
 
-            node = self.all_quad_node[node_index]
-            node_index = node_index_to_process.pop()
-
-            num_empty_leaves = 0
-            for i in range(4):
-
-                child_index = node.first_child + i
-
-                child_node  = self.all_quad_node[child_index]
-
-                if child_node.total_node == 0:
-                    num_empty_leaves += 1
-                elif child_node.total_node == -1:
-                    node_index_to_process.append(child_index)
-      
-            if num_empty_leaves == 4:
-                # set the first_child of the child node to the free_node_index
-                # set the free node's index to the child node
-                self.all_quad_node[node.first_child].first_child = self._free_node_index
-                self._free_node_index = node.first_child
-
-                # Make this node the new empty leaf.
-                node.first_child = -1
-                node.total_node  = 0
+        traversal_clean_up(self.root)
 
 
     def clear(self, cache=False) -> None:
-        [self.delete(entity.pos) for entity in self.traverse_entity()]
-        self.cleanup()
+        [self.delete(eid) for eid in self.all_entity.keys()]
+        self.all_entity.clear()
+        self.clean_up()
 
 
-    def set_free(self, node: Union[QuadNode, QuadEntityNode]) -> None:
-        if isinstance(node, QuadEntityNode):
-            owner_node = node.owner_node
-            siblings = self.find_entity_node(qnode=owner_node)
-            node_relative_index = siblings.index(node)
+    def set_free_entity_nodes(self, indexed_entity_nodes: List[Tuple[int, QuadEntityNode]]) -> None:
+        owner_node = indexed_entity_nodes[0][1].owner_node
+        owner_node.total_entity = 0
+        owner_node.first_child  = -1
+        
+        self.num_entity_node_in_use -= len(indexed_entity_nodes)
 
-            if node_relative_index != 0:
-                siblings[node_relative_index - 1].next_index = node.next_index
-            else:
-                owner_node.first_child = node.next_index
+        for ind, en in indexed_entity_nodes:
+            en.update(
+                next_index=self._free_entity_node_index, 
+                entity_id=-1, 
+                owner_node=None
+                )
+            self._free_entity_node_index = ind
 
-            node.update(next_index=self._free_entity_node_index, entity_id=-1, owner_node=None)
-            self._free_entity_node_index = self.all_entity_node.index(node)
 
-            owner_node.total_entity -= 1
-            return
+    def set_free_quad_node(self, node: Union[QuadNode, QuadEntityNode]) -> None:
 
         node_index = self.all_quad_node.index(node)
-        if self.all_quad_node[node.parent_index].first_child != node_index:
+        parent_node = self.all_quad_node[node.parent_index]
+
+        if parent_node.first_child != node_index:
             raise ValueError(f"the liberation should happen with the first child not the '{node_index % 4}' child")
 
-        node.update(first_child=self._free_node_index, total_entity=-1, parent_index=-1)
-        self._free_node_index = node_index
+        for i in range(4):
+            child_index = parent_node.first_child + i
+            self.all_quad_node[child_index].update(
+                first_child=self._free_quad_node_index, 
+                total_entity=-1, 
+                parent_index=-2
+                )
+
+        self._free_quad_node_index = node_index
+        self.num_quad_node_in_use -= 4
+        parent_node.first_child  = -1
+        parent_node.total_entity = 0
 
 
     def add_entity_node(self, node, entity_id):
-        old_entity_node_index = node.first_child
+        old_entity_node_index = node.first_child if node.total_entity > 0 else -1
 
         # update the node's total bounded entity
         node.total_entity += 1
 
-        if self._free_entity_node_index == -1:
+        self.num_entity_node_in_use += 1
 
+        if self._free_entity_node_index == -1:
             node.first_child = len(self.all_entity_node)
             self.all_entity_node.append(
                 QuadEntityNode(
@@ -393,7 +383,6 @@ class QuadTree:
                 )
             return 
 
-        # get 1st of the four free entity node
         free_entity_node = self.all_entity_node[self._free_entity_node_index]
 
         # set the node's child to the free entity node
@@ -401,30 +390,42 @@ class QuadTree:
 
         # reset the free entity node's index
         self._free_entity_node_index = free_entity_node.next_index
-
+        
         # update the newly allocated entity node's attribute
-        free_entity_node.update(entity_id=entity_id, next_index=old_entity_node_index, owner_node=node) 
+        free_entity_node.update(
+            entity_id=entity_id, 
+            next_index=old_entity_node_index, 
+            owner_node=node
+            ) 
 
 
     def set_branch(self, node):
-        node.total_entity = -1
         node_index = self.all_quad_node.index(node)
 
-        if self._free_node_index == -1:
+        node.total_entity = -1
+
+        self.num_quad_node_in_use += 4
+
+        if self._free_quad_node_index == -1:
             # create 4 more quad node's and set the node's first child index to it
             node.first_child = len(self.all_quad_node) 
             [self.all_quad_node.append(QuadNode(parent_index=node_index)) for i in range(4)]
             return
 
         # allocate that free node to the current node
-        node.first_child = self._free_node_index
+        node.first_child = self._free_quad_node_index
 
-        self.all_quad_node[node.first_child].parent_index = node_index
+        next_free_quad_node_index = self.all_quad_node[node.first_child].first_child
+
+        for i in range(4):
+            child_index = self._free_quad_node_index + i
+            self.all_quad_node[child_index].update(first_child=-1, total_entity=0, parent_index=node_index)
+
         # reset the free node's index to the free node's child
         # which should either be -1 if there's no more free node or another node's index
-        self._free_node_index = self.all_quad_node[node.first_child].first_child
+        self._free_quad_node_index = next_free_quad_node_index
 
-    
+
     def insert(self, entity_bbox: BBox, entity_id: Optional['UID']=None) -> None:
         if not isinstance(entity_bbox, (type(None), tuple, BBox)) or \
            not all(isinstance(num, (int, float)) for num in entity_bbox):
@@ -433,6 +434,9 @@ class QuadTree:
         if isinstance(entity_id, type(None)) and not self.auto_id:
             raise ValueError(f"set QuadTree's auto_id to True or provide ids for the entity") 
 
+        if self.depth > self.max_depth:
+            raise ValueError(f"Quadtree's depth has exceeded the maximum depth of {self.max_depth}")
+
         if isinstance(entity_id, type(None)): 
             entity_id = len(self.all_entity)
 
@@ -440,17 +444,17 @@ class QuadTree:
             entity_bbox = BBox(*entity_bbox)
 
         self.all_entity[entity_id] = entity_bbox
- 
-        # entity nodes 
+
         for node, bbox in self.find_leaves(bbox=entity_bbox):
 
             self.add_entity_node(node, entity_id)
 
             if node.total_entity > self.capacity: 
-                entity_nodes = self.find_entity_node(qnode=node)
-                entity_ids   = [en.entity_id for en in entity_nodes]
 
-                [self.set_free(en) for en in entity_nodes]
+                entity_nodes = self.find_entity_node(qnode=node, index=True)
+                entity_ids   = [en.entity_id for _, en in entity_nodes]
+
+                self.set_free_entity_nodes(entity_nodes)
 
                 self.set_branch(node)
 
@@ -458,16 +462,4 @@ class QuadTree:
 
 
     def __len__(self) -> int:
-        return sum(node.total_entity for node in self.all_quad_node if node.total_entity > 0)
-
-
-a = QuadTree((0, 0, 1000, 1000), auto_id=True, capacity=1)
-a.insert((1,1))
-a.insert((3,3))
-a.insert((100, 100))
-a.insert((990, 50))
-
-for i in a.all_entity.keys():
-    a.delete(i)
-
-print(len(a))
+        return sum(node.total_entity for node in self.all_quad_node if node.in_use and node.is_leaf)
